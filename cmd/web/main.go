b95e8c7321c344ad47521e8eeb7ae3eda8b53410
Stephen Montague <me@montaguestephen.com>
2021-11-23 09:03:20 -0500

    Test files (WIP)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,15 +3,14 @@ package main
 import (
 	"encoding/gob"
 	"fmt"
-	"log"
-	"net/http"
-	"time"
-
 	"github.com/alexedwards/scs/v2"
 	"github.com/stephenmontague/go-bnb/internal/config"
 	"github.com/stephenmontague/go-bnb/internal/handlers"
 	"github.com/stephenmontague/go-bnb/internal/models"
 	"github.com/stephenmontague/go-bnb/internal/render"
+	"log"
+	"net/http"
+	"time"
 )
 
 const portNumber = ":8080"
@@ -20,9 +19,26 @@ var session *scs.SessionManager
 
 // main is the main application function
 func main() {	
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Starting application on port %s \n", portNumber)
+
+	srv := &http.Server{
+		Addr: portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
+
+func run() error {
 	// what am I going to put in the session
 	gob.Register(models.Reservation{})
-	
+
 	// change this to true when in production
 	app.InProduction = false
 
@@ -34,10 +50,11 @@ func main() {
 	session.Cookie.Secure = app.InProduction // In production we want this to be true. False for dev b/c localhost is not secure
 
 	app.Session = session
-	
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache", err)
+		return err
 	}
 
 	app.TemplateCache = tc
@@ -48,13 +65,5 @@ func main() {
 
 	render.NewTemplates(&app)
 
-	fmt.Printf("Starting application on port %s \n", portNumber)
-
-	srv := &http.Server{
-		Addr: portNumber,
-		Handler: routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-	log.Fatal(err)
+	return nil
 }
